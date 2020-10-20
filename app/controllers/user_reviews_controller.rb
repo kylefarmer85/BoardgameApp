7c2eb206f56504972994692c02f2877e0d17ac70
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-20 18:33:23 -0500

    Added user reviews and ratings to boardgame show page, also added average rating, added validations to user reviews/ratings

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -1,2 +1,8 @@
 class UserReviewsController < ApplicationController
+
+  def create
+    @user_review = UserReview.create(user_id: current_user.id, boardgame_id: params[:boardgame_id], review: params[:review], rating: params[:rating])
+    flash[:errors] = @user_review.errors.full_messages
+    redirect_to boardgame_path(params[:boardgame_id])
+  end
 end
