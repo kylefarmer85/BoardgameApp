e614518d2ebc5cd7d0d85dde01c9a3bebdd14aa3
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-20 15:48:31 -0500

    Added button to add game to collection, with validation and errors

--- app/controllers/user_boardgames_controller.rb
+++ app/controllers/user_boardgames_controller.rb
@@ -5,7 +5,11 @@ class UserBoardgamesController < ApplicationController
         @user = current_user
     end
 
-    def add_to_owned
+    def create
+        @user_boardgame = UserBoardgame.create(user_id: current_user.id, boardgame_id: params[:boardgame_id])
+        flash[:errors] = @user_boardgame.errors.full_messages
+        redirect_to user_boardgame_path(current_user)
+        
 
     end
 
