c28d9f091976f151a13d36836d77fa0e097d4c1f
john <jmlovitsch@gmail.com>
2020-10-22 15:08:34 -0500

    fixed profile bug

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,35 +1,34 @@
 class Boardgame < ApplicationRecord
-    has_many :boardgame_categories
-    has_many :categories, through: :boardgame_categories
-    has_many :user_reviews
-    has_many :user_boardgames
-    has_many :users, through: :user_boardgames
+  has_many :boardgame_categories
+  has_many :categories, through: :boardgame_categories
+  has_many :user_reviews
+  has_many :user_boardgames
+  has_many :users, through: :user_boardgames
 
-    def self.search(search)
-        if search
-            boardgame = Boardgame.find_by(name: search.titlecase)
-            if boardgame
+  def self.search(search)
+    if search
+      boardgame = Boardgame.find_by(name: search.titlecase)
+      if boardgame
 
-                Boardgame.where("name LIKE ?", "%" + search + "%") #+
-                #Boardgame.where("name LIKE ?", "%" + search.first(3) + "%").limit(10)
-            else
-                Boardgame.where("name LIKE ?", "%" + search.first(3) + "%").limit(10)
-            end
+        Boardgame.where('name LIKE ?', '%' + search + '%') #+
+      # Boardgame.where("name LIKE ?", "%" + search.first(3) + "%").limit(10)
+      else
+        Boardgame.where('name LIKE ?', '%' + search.first(3) + '%').limit(10)
+      end
+    end
+  end
+
+  def avg_rating
+    if user_reviews.count > 0
+      sum = 0
+      count = 0
+      user_reviews.each do |user_review|
+        unless user_review.rating.nil?
+          sum += user_review.rating
+          count += 1
         end
+      end
+      sum / count
     end
-
-    def avg_rating
-        if self.user_reviews.count > 0
-        sum = 0
-        count = 0
-        self.user_reviews.each do |user_review|
-            if user_review.rating != nil
-            sum += user_review.rating
-            count += 1
-            end
-        end
-        sum / count
-    end
-    end
-
+  end
 end
